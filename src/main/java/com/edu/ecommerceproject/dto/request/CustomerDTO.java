65754932cd988b837e0da449061d7d3633868ad1
Hicham Hared <hichamharedsm@gmail.com>
2025-07-28 03:55:40 +0100

    feat(dto): add CustomerDTO and SessionDTO for request handling

--- src/main/java/com/edu/ecommerceproject/dto/request/CustomerDTO.java
+++ src/main/java/com/edu/ecommerceproject/dto/request/CustomerDTO.java
@@ -2,10 +2,6 @@ package com.edu.ecommerceproject.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
-
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
