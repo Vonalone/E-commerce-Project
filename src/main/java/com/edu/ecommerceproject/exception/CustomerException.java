590f752268b56a03c096df8a6c85f6bb9f59a6e2
Hicham Hared <hichamharedsm@gmail.com>
2025-07-28 03:52:03 +0100

    feat(customer): add custom CustomerException for business logic errors

---
+++ src/main/java/com/edu/ecommerceproject/exception/CustomerException.java
@@ -0,0 +1,7 @@
+package com.edu.ecommerceproject.exception;
+
+public class CustomerException extends RuntimeException {
+    public CustomerException(String message) {
+        super(message);
+    }
+}
