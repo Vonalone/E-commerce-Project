618cd0e6d2f30e94eb7862a728bea11f1cf376e4
Hicham Hared <hichamharedsm@gmail.com>
2025-07-28 13:12:56 +0100

    feat(session): add custom method findByUserId and findByToken

--- src/main/java/com/edu/ecommerceproject/repository/SessionRepository.java
+++ src/main/java/com/edu/ecommerceproject/repository/SessionRepository.java
@@ -5,6 +5,11 @@ import com.edu.ecommerceproject.models.entities.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SessionRepository extends JpaRepository<Session,Integer> {
+    Optional<Session> findByUserId(Integer userId);
+    Optional<Session> findByToken(String token);
 }
