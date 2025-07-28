c954c6d49edb71199befca9ea001f3234f2bf057
Hicham Hared <hichamharedsm@gmail.com>
2025-07-28 13:17:37 +0100

    feat(auth): add /login/customer endpoint and /logout/endpoint with create session token

--- src/main/java/com/edu/ecommerceproject/controller/AuthController.java
+++ src/main/java/com/edu/ecommerceproject/controller/AuthController.java
@@ -1,10 +1,14 @@
 package com.edu.ecommerceproject.controller;
+import com.edu.ecommerceproject.dto.request.CustomerDTO;
+import com.edu.ecommerceproject.dto.request.SessionDTO;
 import com.edu.ecommerceproject.models.entities.Customer;
+import com.edu.ecommerceproject.models.entities.Session;
 import com.edu.ecommerceproject.service.IAuthService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,4 +27,17 @@ public class AuthController {
      return new ResponseEntity<>(authService.registerCustomer(customer), HttpStatus.CREATED);
 
     }
+
+
+    @PostMapping(value = "/login/customer",consumes = "application/json")
+    public ResponseEntity<Session> loginCustomer(@Valid @RequestBody CustomerDTO customerDTO){
+        return  new ResponseEntity<>(authService.loginCustomer(customerDTO),HttpStatus.ACCEPTED);
+    }
+
+   @PostMapping(value = "/logout/customer" ,consumes = "application/json")
+    public ResponseEntity<SessionDTO> logoutCustomer(@Valid @RequestBody SessionDTO sessionDTO){
+        return new ResponseEntity<>(authService.logoutCustomer(sessionDTO),HttpStatus.ACCEPTED);
+   }
+
+
 }
