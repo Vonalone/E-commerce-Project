ab4493fc66e7c04b66df3e62e5418fcbea4adc93
Hicham Hared <hichamharedsm@gmail.com>
2025-07-28 03:57:51 +0100

    feat(customer): setup ICustomerService and CustomerServiceImpl and CustomerController structure

---
+++ src/main/java/com/edu/ecommerceproject/service/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.edu.ecommerceproject.service;
+
+import com.edu.ecommerceproject.models.entities.Customer;
+
+public interface ICustomerService {
+
+}
---
+++ src/main/java/com/edu/ecommerceproject/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,8 @@
+package com.edu.ecommerceproject.service.impl;
+
+import com.edu.ecommerceproject.service.ICustomerService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerServiceImpl implements ICustomerService {
+}
