5fa8ea45d99bdb6e4ce068b48b0b082d1f4f41ed
Hicham Hared <hichamharedsm@gmail.com>
2025-07-28 22:02:25 +0100

    feat(auth): add new endpoint to AuthController /register/seller endpoint

--- src/main/java/com/edu/ecommerceproject/controller/AuthController.java
+++ src/main/java/com/edu/ecommerceproject/controller/AuthController.java
@@ -2,13 +2,13 @@ package com.edu.ecommerceproject.controller;
 import com.edu.ecommerceproject.dto.request.CustomerDTO;
 import com.edu.ecommerceproject.dto.request.SessionDTO;
 import com.edu.ecommerceproject.models.entities.Customer;
+import com.edu.ecommerceproject.models.entities.Seller;
 import com.edu.ecommerceproject.models.entities.Session;
 import com.edu.ecommerceproject.service.IAuthService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +38,13 @@ public class AuthController {
     public ResponseEntity<SessionDTO> logoutCustomer(@Valid @RequestBody SessionDTO sessionDTO){
         return new ResponseEntity<>(authService.logoutCustomer(sessionDTO),HttpStatus.ACCEPTED);
    }
+
+    // handle seller login system
+
+    @PostMapping(value = "/register/seller",consumes = "application/json")
+    public ResponseEntity<Seller> registerSeller(@Valid @RequestBody Seller seller){
+        return new ResponseEntity<>(authService.registerSeller(seller),HttpStatus.CREATED);
+    }
 
 
 }
