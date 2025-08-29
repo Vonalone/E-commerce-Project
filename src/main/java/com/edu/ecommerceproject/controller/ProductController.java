d230a9347f518ff35fbda8e8bab1d08e81d03479
Hicham Hared <hichamharedsm@gmail.com>
2025-08-29 14:15:08 +0100

    feat(Product): add endpoints for get product with filter and update quantity product

--- src/main/java/com/edu/ecommerceproject/controller/ProductController.java
+++ src/main/java/com/edu/ecommerceproject/controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.edu.ecommerceproject.controller;
 
 
+import com.edu.ecommerceproject.dto.request.ProductDTO;
 import com.edu.ecommerceproject.models.entities.Product;
+import com.edu.ecommerceproject.models.enums.CategoryEnum;
+import com.edu.ecommerceproject.models.enums.ProductStatusEnum;
 import com.edu.ecommerceproject.service.IProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -36,6 +39,22 @@ public class ProductController {
     @PutMapping
     public ResponseEntity<Product> updateProduct(@Valid @RequestBody Product product){
         return new ResponseEntity<>(productService.updateProduct(product),HttpStatus.OK);
+    }
+    @GetMapping("/category/{categoryEnum}")
+    public ResponseEntity<List<ProductDTO>> getAllProductOfCategory(@PathVariable CategoryEnum categoryEnum){
+        return new ResponseEntity<>(productService.getAllProductsOfCategory(categoryEnum),HttpStatus.OK);
+    }
+    @GetMapping("/seller/{id}")
+    public ResponseEntity<List<ProductDTO>> getAllProductOfSeller(@PathVariable Integer id){
+        return new ResponseEntity<>(productService.getAllProductOfSeller(id),HttpStatus.OK);
+    }
+    @GetMapping("/status/{productStatusEnum}")
+    public ResponseEntity<List<ProductDTO>> getAllProductOfStatus(@PathVariable ProductStatusEnum productStatusEnum){
+        return new ResponseEntity<>(productService.getAllProductsOfStatus(productStatusEnum),HttpStatus.OK);
+    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<Product> updateProductQuantity(@PathVariable Integer id,@RequestBody ProductDTO productDTO){
+        return new ResponseEntity<>(productService.updateProductQuantityById(id,productDTO),HttpStatus.OK);
     }
 
 
