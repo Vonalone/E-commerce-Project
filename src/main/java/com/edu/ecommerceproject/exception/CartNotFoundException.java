4cdbddd989b0ad88b0d2e16d5255ed5239973440
Hicham Hared <hichamharedsm@gmail.com>
2025-08-30 21:20:41 +0100

    feat(Cart): custom exception for not found cart exception

---
+++ src/main/java/com/edu/ecommerceproject/exception/CartNotFoundException.java
@@ -0,0 +1,7 @@
+package com.edu.ecommerceproject.exception;
+
+public class CartNotFoundException extends RuntimeException {
+    public CartNotFoundException(String message) {
+        super(message);
+    }
+}
